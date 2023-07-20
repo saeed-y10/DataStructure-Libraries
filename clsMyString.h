a035d6e0a4d3021f91f6e3c4f714e3a0876e77d3
saeed <saeed_99_per@outlook.com>
2023-07-20 20:40:02 +0300

    create undo and redo method in strings using stack

---
+++ clsMyString.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+class clsMyString
+{
+
+private:
+
+	string _Value;
+	stack<string> _Undo;
+	stack<string> _Redo;
+
+public:
+
+	void Set(string value)
+	{
+		_Undo.push(_Value);
+		_Value = value;
+	}
+
+	string Get()
+	{
+		return _Value;
+	}
+	__declspec(property(get = Get, put = Set)) string Value;
+
+	void Undo()
+	{
+		if (!_Undo.empty())
+		{
+			_Redo.push(_Value);
+			_Value = _Undo.top();
+			_Undo.pop();
+		}
+	}
+
+	void Redo()
+	{
+		if (!_Redo.empty())
+		{
+			_Undo.push(_Value);
+			_Value = _Redo.top();
+			_Redo.pop();
+		}
+	}
+
+};
+
